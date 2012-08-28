71c473169ed98fe128b5933d996c829b79ebc7c0
root <root@rocky-mountain.(none)>
2012-08-27 18:12:56 -0700

    remove non-source files

--- src-2/.tmp_versions/vdfs.mod
+++
@@ -1,2 +0,0 @@
-/home/vinay/Desktop/vdfs/src-2/vdfs.ko
-/home/vinay/Desktop/vdfs/src-2/vdfs.o
--- src-2/.vdfs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vinay/Desktop/vdfs/src-2/vdfs.ko := ld -r -m elf_x86_64 -T /usr/src/linux-3.3.2/scripts/module-common.lds --build-id  -o /home/vinay/Desktop/vdfs/src-2/vdfs.ko /home/vinay/Desktop/vdfs/src-2/vdfs.o /home/vinay/Desktop/vdfs/src-2/vdfs.mod.o
